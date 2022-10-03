d8af173bd34fdcf9734ce37b2ca103d613ee00cb
Daniel Madrid <danimadgom@gmail.com>
2022-10-03 14:37:06 +0200

    Primera confirmacion de cambios

---
+++ src/keyevent/KeyEvent.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package keyevent;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author usu2dam
+ */
+public class KeyEvent extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        Text text = new Text(20, 20, "A");
+        text.setFocusTraversable(true);
+        pane.getChildren().add(text);
+        text.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case DOWN: text.setY(text.getY() + 10); break;
+                case UP: text.setY(text.getY() - 10); break;
+                case LEFT: text.setX(text.getX() - 10); break;
+                case RIGHT: text.setX(text.getX() + 10); break;
+                default:
+                    if (e.getCode().isDigitKey() || e.getCode().isLetterKey())
+                        text.setText(e.getText());
+            }
+        });
+        
+        Scene scene = new Scene(pane, 300, 250);
+        
+        primaryStage.setTitle("Key Event");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
